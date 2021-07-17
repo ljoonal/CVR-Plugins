9af5eddc67c6576d98db035fc2f05521a10f9400
ljoonal <ljoonal@ljoonal.xyz>
2021-07-17 12:45:11 +0300

    Finished HWID spoofer

--- SpoofHWID/SpoofHWID.cs
+++ SpoofHWID/SpoofHWID.cs
@@ -22,7 +22,6 @@ namespace SpoofHWID
 		private const string ConfigCategory = "Settings";
 		private static ConfigEntry<string> ConfigHWID;
 
-
 		SpoofHWIDMod()
 		{
 			const string settingName = "HWID";
@@ -32,26 +31,32 @@ namespace SpoofHWID
 				"",
 				"The HWID to use. If it's not the correct length (by being empty for example) a new one will be generated.");
 
-			// Following is mostly from knah's similar mod for MelonLoader, licensed under GPL3.0
-			// Code & License can be found from https://github.com/knah/ML-UniversalMods
+
 			if (ConfigHWID.Value.Length != SystemInfo.deviceUniqueIdentifier.Length)
 			{
-				var random = new System.Random(Environment.TickCount);
-				var bytes = new byte[SystemInfo.deviceUniqueIdentifier.Length / 2];
-				random.NextBytes(bytes);
-				ConfigHWID.Value = string.Join("", bytes.Select(it => it.ToString("x2")));
-				//ConfigHWID.Value.Save();
+				Logger.LogInfo("Generating new HWID");
+				ConfigHWID.Value = RandomHWID();
 			}
+		}
+
+		// Following is mostly from knah's similar mod for MelonLoader, licensed under GPL3.0
+		// Code & License can be found from https://github.com/knah/ML-UniversalMods
+		private string RandomHWID()
+		{
+			var random = new System.Random(Environment.TickCount);
+			var bytes = new byte[SystemInfo.deviceUniqueIdentifier.Length / 2];
+			random.NextBytes(bytes);
+			return string.Join("", bytes.Select(it => it.ToString("x2")));
 		}
 
 		void Awake()
 		{
 			try
 			{
+				Logger.LogInfo($"HWID before patch: {SystemInfo.deviceUniqueIdentifier}");
 				Harmony.CreateAndPatchAll(typeof(SpoofHWIDMod));
-				Logger.LogInfo("Spoofed HWID; below two should match:");
-				Logger.LogInfo($"Current: {SystemInfo.deviceUniqueIdentifier}");
-				Logger.LogInfo($"Target:  {ConfigHWID.Value}");
+				Logger.LogInfo($"HWID after patch: {SystemInfo.deviceUniqueIdentifier}");
+				Logger.LogInfo($"HWID target:  {ConfigHWID.Value}");
 			}
 			catch (Exception ex)
 			{
@@ -59,12 +64,12 @@ namespace SpoofHWID
 			}
 		}
 
-		[HarmonyPatch(typeof(SystemInfo), "deviceUniqueIdentifier")] // Specify target method with HarmonyPatch attribute
-		[HarmonyPrefix]                              // There are different patch types. Prefix code runs before original code
+		[HarmonyPatch(typeof(SystemInfo), "deviceUniqueIdentifier", MethodType.Getter)]
+		[HarmonyPrefix]
 		static bool SpoofHWID(ref string __result)
 		{
-			__result = ConfigHWID.Value; // The special __result variable allows you to read or change the return value
-			return false; // Returning false in prefix patches skips running the original code
+			__result = ConfigHWID.Value;
+			return false; // Skip running the original code
 		}
 	}
 }
