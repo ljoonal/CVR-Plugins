101a107f76ffd9c1d8383d22006ee1e0ce83a33e
ljoonal <ljoonal@ljoonal.xyz>
2021-08-03 04:56:51 +0300

    More implementation to a more working state now

--- PlayerRotater/PlayerRotater.cs
+++ PlayerRotater/PlayerRotater.cs
@@ -19,15 +19,17 @@ namespace PlayerRotater
 		private static PlayerRotaterPlugin Instance;
 		private ConfigEntry<KeyboardShortcut> mouseModeToggleKeybind;
 		private ConfigEntry<KeyCode> mouseModeHoldKey;
-		private GameObject playerAvatar
+		private Vector3? originalRotation = null;
+		private Transform playerAvatarTransform
 		{
 			get
 			{
-				return ABI_RC.Core.RootLogic.Instance.localPlayerAvatarObject;
+				return ABI_RC.Core.Player.PlayerSetup.Instance._movementSystem.transform;
 			}
 		}
 
-		private EnabledState _mouseLookEnabledField;
+		private EnabledState _mouseLookEnabledField = EnabledState.Off;
+
 		private EnabledState mouseLookEnabled
 		{
 			get
@@ -36,9 +38,9 @@ namespace PlayerRotater
 			}
 			set
 			{
-				// TODO; Reset player rotation when turned off.
 				if (_mouseLookEnabledField != value)
 					Logger.LogInfo($"Mouse look set to {value}");
+
 				_mouseLookEnabledField = value;
 			}
 		}
@@ -50,13 +52,13 @@ namespace PlayerRotater
 			mouseModeToggleKeybind = Config.Bind(
 				inputPrefsCategory,
 				"KeybindMouseMode",
-				new KeyboardShortcut(KeyCode.F, new KeyCode[] { KeyCode.LeftControl }),
+				new KeyboardShortcut(KeyCode.None),
 				"The keybind for toggling mouse mode");
 
 			mouseModeHoldKey = Config.Bind(
 				inputPrefsCategory,
 				"KeycodeMouseModeHold",
-				KeyCode.None,
+				KeyCode.Mouse2,
 				"A key that enables mouse mode when holding it down.");
 
 			Instance = this;
@@ -64,7 +66,6 @@ namespace PlayerRotater
 			try
 			{
 				Harmony.CreateAndPatchAll(typeof(PlayerRotaterPlugin));
-				Logger.LogInfo("Started successfully!");
 			}
 			catch (System.Exception ex)
 			{
@@ -72,43 +73,64 @@ namespace PlayerRotater
 			}
 		}
 
-		private void RotatePlayer(float pitch = 0f, float roll = 0f, float yaw = 0f)
+		private void rotatePlayer(float pitch = 0f, float roll = 0f, float yaw = 0f)
 		{
-			playerAvatar.transform.Rotate(new Vector3(pitch, yaw, roll));
+			playerAvatarTransform.Rotate(new Vector3(pitch, yaw, roll));
 		}
 
 		// A patch to handle mouse mode.
 		[HarmonyPatch(typeof(ABI_RC.Core.Savior.InputModuleMouseKeyboard), "UpdateInput")]
 		[HarmonyPostfix]
-		static void InputPatches()
+		static void InputPatch()
 		{
-			// Only allow rotation whilst flying.
+			Instance.OnUpdateInput();
+		}
+
+		private void OnUpdateInput()
+		{
+			// Don't touch rotations if CVR doesn't want us to currently.
+			if (!ABI_RC.Core.Player.PlayerSetup.Instance._movementSystem.canRot) return;
+
+			// Only use rotation whilst flying.
 			if (!ABI_RC.Core.Player.PlayerSetup.Instance._movementSystem.flying)
 			{
-				Instance.mouseLookEnabled = EnabledState.Off;
+				mouseLookEnabled = EnabledState.Off;
+				if (originalRotation is not null)
+				{
+					playerAvatarTransform.eulerAngles = new Vector3(
+						originalRotation.Value.x,
+						playerAvatarTransform.eulerAngles.y,
+						originalRotation.Value.z
+					);
+					originalRotation = null;
+				}
 				return;
 			}
 
-			if (Instance.mouseModeToggleKeybind.Value.IsDown())
-			{
-				if (Instance.mouseLookEnabled == EnabledState.Toggled)
-					Instance.mouseLookEnabled = EnabledState.Off;
-				else Instance.mouseLookEnabled = EnabledState.Toggled;
-			}
-			else if (Input.GetKey(Instance.mouseModeHoldKey.Value))
-				Instance.mouseLookEnabled = EnabledState.Holding;
-			else if (Instance.mouseLookEnabled == EnabledState.Holding)
-				Instance.mouseLookEnabled = EnabledState.Off;
+			if (originalRotation is null) originalRotation = playerAvatarTransform.eulerAngles;
+
+			processMouseLookState();
 
 			// After this everything is for when mouse look is enabled.
-			if (Instance.mouseLookEnabled == EnabledState.Off) return;
+			if (mouseLookEnabled == EnabledState.Off) return;
 
-			Instance.RotatePlayer(
-				pitch: ABI_RC.Core.Savior.CVRInputManager.Instance.lookVector.y,
-				yaw: ABI_RC.Core.Savior.CVRInputManager.Instance.lookVector.x
+			rotatePlayer(
+				pitch: ABI_RC.Core.Savior.CVRInputManager.Instance.lookVector.y * -1,
+				roll: ABI_RC.Core.Savior.CVRInputManager.Instance.lookVector.x
 			);
 
 			ABI_RC.Core.Savior.CVRInputManager.Instance.lookVector = Vector2.zero;
+		}
+
+		private void processMouseLookState()
+		{
+			if (mouseModeToggleKeybind.Value.IsDown())
+			{
+				if (mouseLookEnabled == EnabledState.Toggled) mouseLookEnabled = EnabledState.Off;
+				else mouseLookEnabled = EnabledState.Toggled;
+			}
+			else if (Input.GetKey(mouseModeHoldKey.Value)) mouseLookEnabled = EnabledState.Holding;
+			else if (mouseLookEnabled == EnabledState.Holding) mouseLookEnabled = EnabledState.Off;
 		}
 	}
 }
