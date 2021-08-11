042ac975ab6833e5056e159104bc727d0c25c693
ljoonal <ljoonal@ljoonal.xyz>
2021-08-12 01:10:07 +0300

    Make rotations less jank overall

--- PlayerRotater/PlayerRotater.cs
+++ PlayerRotater/PlayerRotater.cs
@@ -2,6 +2,10 @@ using BepInEx;
 using BepInEx.Configuration;
 using UnityEngine;
 using HarmonyLib;
+using CVRInputManager = ABI_RC.Core.Savior.CVRInputManager;
+using InputModuleMouseKeyboard = ABI_RC.Core.Savior.InputModuleMouseKeyboard;
+using PlayerSetup = ABI_RC.Core.Player.PlayerSetup;
+using CVR_MovementSystem = ABI_RC.Core.Player.CVR_MovementSystem;
 
 namespace PlayerRotater
 {
@@ -20,11 +24,11 @@ namespace PlayerRotater
 		private ConfigEntry<KeyboardShortcut> mouseModeToggleKeybind;
 		private ConfigEntry<KeyCode> mouseModeHoldKey;
 		private Vector3? originalRotation = null;
-		private Transform PlayerAvatarTransform
+		private Transform PlayerRotationTransform
 		{
 			get
 			{
-				return ABI_RC.Core.Player.PlayerSetup.Instance._movementSystem.transform;
+				return PlayerSetup.Instance._movementSystem.transform;
 			}
 		}
 
@@ -75,13 +79,15 @@ namespace PlayerRotater
 
 		private void RotatePlayer(float pitch = 0f, float roll = 0f, float yaw = 0f)
 		{
-			PlayerAvatarTransform.Rotate(new Vector3(pitch, yaw, roll));
+			if (pitch == 0f && roll == 0f && yaw == 0f) return;
+			float m = Traverse.Create(PlayerSetup.Instance._movementSystem).Field("RotationMultiplier").GetValue<float>();
+			PlayerRotationTransform.Rotate(new Vector3(pitch * m, yaw * m, roll * m));
 		}
 
 		// A patch to handle mouse mode.
-		[HarmonyPatch(typeof(ABI_RC.Core.Savior.InputModuleMouseKeyboard), "UpdateInput")]
+		[HarmonyPatch(typeof(InputModuleMouseKeyboard), "UpdateInput")]
 		[HarmonyPostfix]
-		public static void InputPatch()
+		private static void InputPatch()
 		{
 			Instance.OnUpdateInput();
 		}
@@ -89,17 +95,17 @@ namespace PlayerRotater
 		private void OnUpdateInput()
 		{
 			// Don't touch rotations if CVR doesn't want us to currently.
-			if (!ABI_RC.Core.Player.PlayerSetup.Instance._movementSystem.canRot) return;
+			if (!PlayerSetup.Instance._movementSystem.canRot) return;
 
 			// Only use rotation whilst flying.
-			if (!ABI_RC.Core.Player.PlayerSetup.Instance._movementSystem.flying)
+			if (!PlayerSetup.Instance._movementSystem.flying)
 			{
 				MouseLookEnabled = EnabledState.Off;
 				if (originalRotation is not null)
 				{
-					PlayerAvatarTransform.eulerAngles = new Vector3(
+					PlayerRotationTransform.eulerAngles = new Vector3(
 						originalRotation.Value.x,
-						PlayerAvatarTransform.eulerAngles.y,
+						PlayerRotationTransform.eulerAngles.y,
 						originalRotation.Value.z
 					);
 					originalRotation = null;
@@ -107,19 +113,33 @@ namespace PlayerRotater
 				return;
 			}
 
-			if (originalRotation is null) originalRotation = PlayerAvatarTransform.eulerAngles;
-
 			ProcessMouseLookState();
 
-			// After this everything is for when mouse look is enabled.
+			Vector2 lookVector = CVRInputManager.Instance.lookVector;
+
+			if (originalRotation is not null &&
+				!CVRInputManager.Instance.independentHeadTurn &&
+				!CVRInputManager.Instance.independentHeadToggle)
+			{
+				CVRInputManager.Instance.lookVector = Vector2.zero;
+
+				if (MouseLookEnabled == EnabledState.Off)
+				{
+					RotatePlayer(pitch: lookVector.y * -1, yaw: lookVector.x);
+					/*Transform head = PlayerSetup.Instance.animatorManager.getHumanHeadTransform();
+					PlayerRotationTransform.transform.RotateAround(
+						transform.position,
+						head.up,
+						CVRInputManager.Instance.lookVector.x * this.RotationMultiplier
+						);*/
+				}
+			}
+
 			if (MouseLookEnabled == EnabledState.Off) return;
 
-			RotatePlayer(
-				pitch: ABI_RC.Core.Savior.CVRInputManager.Instance.lookVector.y,
-				roll: ABI_RC.Core.Savior.CVRInputManager.Instance.lookVector.x * -1
-			);
+			if (originalRotation is null) originalRotation = PlayerRotationTransform.eulerAngles;
 
-			ABI_RC.Core.Savior.CVRInputManager.Instance.lookVector = Vector2.zero;
+			RotatePlayer(pitch: lookVector.y * -1, roll: lookVector.x * -1);
 		}
 
 		private void ProcessMouseLookState()
