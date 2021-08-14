516f35a8959a4649e77564a32ca1ed51dc32695d
ljoonal <ljoonal@ljoonal.xyz>
2021-08-14 22:17:06 +0300

    Update logging from HopLib changes

--- MALogger/MALogger.cs
+++ MALogger/MALogger.cs
@@ -1,12 +1,9 @@
+using System.Collections.Generic;
 using BepInEx;
 using BepInEx.Configuration;
-using UnityEngine;
-using HarmonyLib;
-using CVRPlayerManager = ABI_RC.Core.Player.CVRPlayerManager;
-using DownloadJob = ABI_RC.Core.IO.DownloadJob;
-using CVRObjectLoader = ABI_RC.Core.IO.CVRObjectLoader;
-using CVRPortalManager = ABI_RC.Core.InteractionSystem.CVRPortalManager;
 using HopLib;
+using CVRPlayerManager = ABI_RC.Core.Player.CVRPlayerManager;
+using CVRPortalManager = ABI_RC.Core.InteractionSystem.CVRPortalManager;
 
 namespace MALogger
 {
@@ -16,7 +13,9 @@ namespace MALogger
 	public class MALoggerPlugin : BaseUnityPlugin
 	{
 		private static MALoggerPlugin Instance;
-		private ConfigEntry<bool> PropLoggingEnabled, AvatarLoggingEnabled, PortalLoggingEnabled;
+		private ConfigEntry<bool> PropLoggingEnabled, AvatarLoggingEnabled, PortalLoggingEnabled, ConnectionLoggingEnabled;
+
+		private readonly Dictionary<string, string> ConnectedPlayerNames = new();
 
 		public void Awake()
 		{
@@ -36,12 +35,38 @@ namespace MALogger
 				"LogPortals",
 				true,
 				"If to dropped portals avatars");
+			ConnectionLoggingEnabled = Config.Bind(
+				settingsCategory,
+				"LogConnections",
+				true,
+				"If to log join and leave messages");
 
 			Instance = this;
 
+			HopApi.PlayerAdded += OnPlayerJoin;
+			HopApi.PlayerRemoved += OnPlayerLeave;
 			HopApi.AvatarLoaded += OnAvatarLoad;
 			HopApi.PropLoaded += OnPropLoad;
 			HopApi.PortalLoaded += OnPortalLoad;
+			HopApi.InstanceDisconnect += delegate { ConnectedPlayerNames.Clear(); };
+		}
+
+		private void OnPlayerLeave(object sender, PlayerIdEventArgs ev)
+		{
+			if (!Instance.ConnectionLoggingEnabled.Value) return;
+			string leaverId = ev.PlayerId;
+			string leaverName = ConnectedPlayerNames[leaverId];
+			Instance.Logger.LogInfo($"Player left: {leaverName} ({leaverId})");
+			ConnectedPlayerNames.Remove(leaverId);
+		}
+
+		private void OnPlayerJoin(object sender, PlayerEventArgs ev)
+		{
+			if (!Instance.ConnectionLoggingEnabled.Value) return;
+			string joinerId = ev.Player.Uuid;
+			string joinerName = ev.Player.PlayerDescriptor.userName;
+			Instance.Logger.LogInfo($"Player joined: {joinerName} ({joinerId})");
+			ConnectedPlayerNames[joinerId] = joinerName;
 		}
 
 		private void OnAvatarLoad(object sender, AvatarEventArgs ev)
@@ -50,7 +75,7 @@ namespace MALogger
 			string avatarGuid = ev.Avatar.GetComponent<ABI.CCK.Components.CVRAssetInfo>().guid;
 			string username = ev.Target?.userName ?? ABI_RC.Core.Savior.MetaPort.Instance.username;
 			string userId = ev.Target?.ownerId ?? ABI_RC.Core.Savior.MetaPort.Instance.ownerId;
-			Instance.Logger.LogInfo($"Avatar {avatarGuid} by {username} ({userId})");
+			Instance.Logger.LogInfo($"Avatar: {avatarGuid} by {username} ({userId})");
 		}
 
 		private void OnPropLoad(object sender, PropEventArgs ev)
@@ -58,7 +83,7 @@ namespace MALogger
 			if (!Instance.PropLoggingEnabled.Value) return;
 			string spawnerId = ev.Prop.SpawnedBy;
 			string spawnerName = CVRPlayerManager.Instance.TryGetPlayerName(spawnerId);
-			Instance.Logger.LogInfo($"Prop {ev.Prop.Spawnable.guid} by {spawnerName} ({spawnerId})");
+			Instance.Logger.LogInfo($"Prop: {ev.Prop.Spawnable.guid} by {spawnerName} ({spawnerId})");
 		}
 
 		private void OnPortalLoad(object sender, PortalEventArgs ev)
@@ -66,7 +91,7 @@ namespace MALogger
 			if (!Instance.PortalLoggingEnabled.Value || ev.Portal.type != CVRPortalManager.PortalType.Instance) return;
 			string dropperId = ev.Portal.portalOwner;
 			string dropperName = CVRPlayerManager.Instance.TryGetPlayerName(dropperId);
-			Instance.Logger.LogInfo($"Portal to {ev.Portal.Portal.PortalName} by {dropperName} ({dropperId})");
+			Instance.Logger.LogInfo($"Portal: {ev.Portal.Portal.PortalName} by {dropperName} ({dropperId})");
 		}
 	}
 }
