4f73480e7b208fe5902b63ff67372cce2ac7ebf0
ljoonal <ljoonal@ljoonal.xyz>
2021-08-14 22:16:19 +0300

    Simplify the mod using HopLib

--- SpeedMultiplier/SpeedMultiplier.cs
+++ SpeedMultiplier/SpeedMultiplier.cs
@@ -1,23 +1,44 @@
 using BepInEx;
+using BepInEx.Configuration;
+using HopLib;
+using Player = ABI_RC.Core.Player;
 
 namespace SpeedMultiplier
 {
-
+	[BepInDependency(HopLibInfo.GUID, HopLibInfo.Version)]
 	[BepInPlugin(BuildInfo.GUID, BuildInfo.Name, BuildInfo.Version)]
 	[BepInProcess("ChilloutVR.exe")]
-	public class SpeedMultiplierPlugin : BaseUnityPlugin
+	class SpeedMultiplierPlugin : BaseUnityPlugin
 	{
+		private ConfigEntry<float> FlyingMultiplier;
+
 		public void Awake()
 		{
-			try
-			{
-				FlyingPatches.Setup(Config);
-			}
-			catch (System.Exception ex)
-			{
-				Logger.LogError($"Failed to apply patch: {ex}");
-			}
+			FlyingMultiplier = Config.Bind(
+				"Speeds",
+				"Flying",
+				5f,
+				"The flying speed multiplier. Set to <= 0 to disable (requires world reload)");
 
+			HopApi.WorldStarted += delegate { ProcessUpdate(); };
+			FlyingMultiplier.SettingChanged += delegate { ProcessUpdate(); };
+		}
+
+		private void ProcessUpdate()
+		{
+#if DEBUG
+			Logger.LogInfo($"{nameof(FlyingMultiplier)}: {FlyingMultiplier.Value}");
+#endif
+			if (FlyingMultiplier.Value > 0)
+				SetFlySpeedMultiplier(FlyingMultiplier.Value);
+		}
+
+		private void SetFlySpeedMultiplier(float speed)
+		{
+#if DEBUG
+			Logger.LogInfo($"Setting flying speed to {speed}");
+#endif
+			Player.CVR_MovementSystem.Instance.floatSpeedMultiplier = speed;
 		}
 	}
 }
