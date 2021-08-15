815e114c2a2fe8d82e26173dcaba49773703f639
ljoonal <ljoonal@ljoonal.xyz>
2021-08-15 06:09:05 +0300

    Add initial additional filters plugin

---
+++ MoreFilters/BuildInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyTitle(MoreFilters.BuildInfo.GUID)]
+[assembly: AssemblyProduct(MoreFilters.BuildInfo.Name)]
+[assembly: AssemblyVersion(MoreFilters.BuildInfo.Version)]
+[assembly: AssemblyCompany("cvr.ljoonal.xyz")]
+
+namespace MoreFilters
+{
+	public static class BuildInfo
+	{
+		public const string GUID = "xyz.ljoonal.cvr.morefilters";
+
+		public const string Name = "More filters";
+
+		public const string Version = "0.0.1";
+	}
+}
---
+++ MoreFilters/MoreFilters.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using BepInEx;
+using BepInEx.Configuration;
+using HopLib;
+using UnityEngine;
+using Friends = ABI_RC.Core.Networking.IO.Social.Friends;
+
+namespace MoreFilters
+{
+	public enum ContentFilterState
+	{
+		Enabled = 0,
+		EnabledForFriends = 1,
+		Disabled = 2
+	}
+
+	[BepInDependency(HopLibInfo.GUID, HopLibInfo.Version)]
+	[BepInPlugin(BuildInfo.GUID, BuildInfo.Name, BuildInfo.Version)]
+	[BepInProcess("ChilloutVR.exe")]
+	class MoreFiltersPlugin : BaseUnityPlugin
+	{
+		protected static bool ShouldFilter(ContentFilterState state, bool isFriend)
+		{
+			return state == ContentFilterState.Disabled ||
+				(state == ContentFilterState.EnabledForFriends && !isFriend);
+		}
+
+		private ConfigEntry<ContentFilterState> FilterSpawnAudio;
+
+		public void Awake()
+		{
+			const string FILTERS_CATEGORY = "Filters";
+			FilterSpawnAudio = Config.Bind(
+				FILTERS_CATEGORY,
+				"SpawnAudio",
+				ContentFilterState.EnabledForFriends,
+				"If spawn audio should be enabled.");
+
+			HopApi.AvatarLoaded += OnAvatarLoaded;
+		}
+
+		private void OnAvatarLoaded(object sender, AvatarEventArgs ev)
+		{
+			if (!ev.IsLocal) StartCoroutine(ProcessAvatarFiltering(ev));
+		}
+
+		private IEnumerator ProcessAvatarFiltering(AvatarEventArgs ev)
+		{
+			bool isFriend = Friends.FriendsWith(ev.Target.ownerId);
+
+			if (!ev.Avatar.activeInHierarchy)
+				yield return new WaitUntil(() => ev.Avatar == null || ev.Avatar.activeInHierarchy);
+
+			if (ev.Avatar == null) yield break;
+
+			if (ShouldFilter(FilterSpawnAudio.Value, isFriend))
+			{
+#if DEBUG
+				Logger.LogInfo($"Filtering audio of {ev.Target.userName}");
+#endif
+				StartCoroutine(RemoveSpawnSounds(ev.Avatar));
+			}
+		}
+
+		private IEnumerator RemoveSpawnSounds(GameObject gameObj)
+		{
+			yield return null;
+			foreach (var audioSource in gameObj.GetComponentsInChildren<AudioSource>(includeInactive: true))
+			{
+#if DEBUG
+				Logger.LogInfo($"Found audio source {audioSource.name}");
+#endif
+				if (audioSource != null && audioSource.isPlaying) audioSource.Stop();
+			}
+		}
+	}
+}
